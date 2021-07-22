8caf1a7fa9a648b6b474f970e32842b8a7e94bbb
Nate Grigg <nate.grigg@endurance.com>
2021-07-22 15:02:57 -0600

    .

---
+++ test_hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import unittest
+from helloer.hello import say_hello
+
+
+class TestHello(unittest.TestCase):
+    def test_none(self):
+        actual = say_hello()
+
+        self.assertEqual('Hello, World', actual)
+
+    def test_empty(self):
+        actual = say_hello('')
+
+        self.assertEqual('Hello, World', actual)
+
+    def test_a_name(self):
+        actual = say_hello('there')
+
+        self.assertEqual('Hello, there', actual)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
